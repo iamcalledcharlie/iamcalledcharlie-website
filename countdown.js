9e045fd516dcbe538312d76c7da6bcf29822341a
iamcalledcharlie <154387957+iamcalledcharlie@users.noreply.github.com>
2023-12-19 19:54:07 -0500

    first commit

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 function updateCountdown() {
-    const endDate = new Date("2023-12-31T00:00:00").getTime(); // Set your launch date and time here
+    const endDate = new Date("2023-12-25T00:00:00").getTime(); // Set your launch date and time here
     const now = new Date().getTime();
     const timeRemaining = endDate - now;
 
@@ -14,5 +14,22 @@ function updateCountdown() {
     document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
 }
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeLetterColors() {
+    const letters = document.querySelectorAll('h1 span');
+    letters.forEach(letter => {
+        letter.style.color = getRandomColor();
+    });
+}
+
 setInterval(updateCountdown, 1000);
+setInterval(changeLetterColors, 500); // Change color every 500ms
 updateCountdown();
